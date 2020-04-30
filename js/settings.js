8eb6a89791ce0c6d4ad5cb9b07e02f956535b1bb
Meli Harvey <harveydee87@gmail.com>
2020-04-29 23:52:41 -0400

    added pill shaped legend

---
+++ js/settings.js
@@ -0,0 +1,43 @@
+var MAPBOX_TOKEN = 'pk.eyJ1IjoiZGNoYXJ2ZXkiLCJhIjoiY2ltemVpNjY1MDRlanVya2szYzlnM2dxcyJ9.im9EDlP7YIYefEt_wz2fww'
+var MAPBOX_STYLE = 'mapbox://styles/dcharvey/ck90r78ib0hnp1jnz9bwleg7h'
+var SIDEWALKS_TILESET = 'mapbox://dcharvey.1typxzdb'
+var SIDEWALKS_LAYER = 'sidewalkwidths_nyc-87ugfr'
+var UNITS = 'ft' // change to 'm' for meters
+var PRECISION = 0.1 // the number of decimal places
+var GROUPS = [
+  {
+    "value": 0,
+    "rating": "impossible",
+    "color": "#FF0049"
+  },
+  {
+    "value": 6,
+    "rating": "very difficult",
+    "color": "#FF461E"
+  },
+  {
+    "value": 9,
+    "rating": "difficult",
+    "color": "#FF9300"
+  },
+  {
+    "value": 12,
+    "rating": "somewhat difficult",
+    "color": "#DAD130"
+  },
+  {
+    "value": 15,
+    "rating": "somewhat easy",
+    "color": "#00ff5a"
+  },
+  {
+    "value": 18,
+    "rating": "easy",
+    "color": "#00FFC4"
+  },
+  {
+    "value": 21,
+    "rating": "very easy",
+    "color": "#00D2FF"
+  },
+]
