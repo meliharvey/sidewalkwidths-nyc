45ebc6d41f12d2407cc5962cdce01caa967452bb
Meli Harvey <meliharvey87@gmail.com>
2024-05-25 14:58:04 -0600

    links to tileset for 2024 sidewalk widths data

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,7 @@
 var MAPBOX_TOKEN = 'pk.eyJ1IjoiZGNoYXJ2ZXkiLCJhIjoiY2s5N3Zjc3ZxMGYwazNlbm9ubzA1d3Q1dCJ9.szxUl4AKCdUNLlmvham6og'
 var MAPBOX_STYLE = 'mapbox://styles/dcharvey/ck90r78ib0hnp1jnz9bwleg7h'
-var SIDEWALKS_TILESET = 'mapbox://dcharvey.1typxzdb'
-var SIDEWALKS_LAYER = 'sidewalkwidths_nyc-87ugfr'
+var SIDEWALKS_TILESET = 'mapbox://dcharvey.8bq5zr3j'
+var SIDEWALKS_LAYER = 'sidewalkwidths_nyc-8ntneu'
 var DISTRICTS_TILESET = 'mapbox://dcharvey.7dbzv200'
 var DISTRICTS_LAYER = 'districts_nyc-1eoi4m'
 var UNITS = 'ft' // change to 'm' for meters
